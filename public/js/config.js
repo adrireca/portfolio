dd6828da6e5a9a599711370147c8475cd1be3923
Adrian <reyes3190@gmail.com>
2023-12-28 21:11:49 +0100

    subida config.js

---
+++ public/js/config.js
@@ -0,0 +1,3 @@
+export const app_id = "UgiSe69sTu9YQigZDcYMeFI6dTs1l7DlMrjXMWB2";
+export const js_key = "G8b7XtnZvPHKKUbGtkTRAN8FvAX8U5KTsjrtdMoa";
+export const host_url = "https://parseapi.back4app.com/";
